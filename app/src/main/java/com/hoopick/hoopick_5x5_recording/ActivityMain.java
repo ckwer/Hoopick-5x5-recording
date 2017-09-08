3684fb126fdf325c824b93ed0171780e76b3da02
JunHyeokLee <skdkak2000@gmail.com>
2017-09-08 10:23:33 +0900

    프로젝트 등록

---
+++ app/src/main/java/com/hoopick/hoopick_5x5_recording/ActivityMain.java
@@ -0,0 +1,13 @@
+package com.hoopick.hoopick_5x5_recording;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
